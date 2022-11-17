e3bda9da0e1b4e6cbd65bfbeaf1cc4ea4f4b4960
ayshafarhan <113276919+ayshafarhan@users.noreply.github.com>
2022-11-17 23:04:37 +0530

    Add files via upload

---
+++ application Building/Build python code/app.py
@@ -0,0 +1,74 @@
+from flask import Flask, app,request,render_template
+from flask import Flask, request, render_template, redirect, url_for
+from cloudant.client import Cloudant
+# Authenticate using an IAM API key
+client = Cloudant.iam('b696605d-6526-4485-bdfa-1c7577fa7d8f-bluemix','uGQKAW_PQI6MXV1f96UyMvEt24wx0IT4lw6ptz-T86sZ', connect=True)
+# Create a database using an initialized client
+my_database = client.create_database('erythema')
+app=Flask(__name__)
+#default home page or route
+@app.route('/')
+def index():
+   return render_template('index.html')
+@app.route('/index.html')
+def home():
+    return render_template("index.html")
+#registration page
+@app.route('/register')
+def register():
+    return render_template('register.html')
+@app.route('/afterreg', methods=['POST'])
+def afterreg():
+    x = [x for x in request.form.values()]
+    print(x)
+    data = {
+    '_id': x[1], # Setting _id is optional
+    'name': x[0],
+    'psw':x[2]
+    }
+    print(data)    
+    query = {'_id': {'$eq': data['_id']}}    
+    docs = my_database.get_query_result(query)
+    print(docs)    
+    print(len(docs.all()))    
+    if(len(docs.all())==0):
+        url = my_database.create_document(data)
+        #response = requests.get(url)
+        return render_template('register.html', pred="Registration Successful, please login using your details")
+    else:
+        return render_template('register.html', pred="You are already a member, please login using your details")
+#login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/afterlogin',methods=['POST'])
+def afterlogin():
+    user = request.form['_id']
+    passw = request.form['psw']
+    print(user,passw)    
+    query = {'_id': {'$eq': user}}        
+    docs = my_database.get_query_result(query)
+    print(docs)    
+    print(len(docs.all()))    
+    if(len(docs.all())==0):
+        return render_template('login.html', pred="The username is not found.")
+    else:
+        if((user==docs[0][0]['_id'] and passw==docs[0][0]['psw'])):
+            return redirect(url_for('prediction'))
+        else:
+            print('Invalid User') 
+    @app.route('/logout')
+def logout():
+    return render_template('logout.html')
+
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html')
+@app.route('/prediction1')
+def prediction1():
+    return render_template('prediction1.html')    
+    #return render_template('prediction.html')     
+    """ Running our application """
+if __name__ == "__main__":
+    app.run(debug=True)
+
